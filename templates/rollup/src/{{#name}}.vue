9272149f202eb1a8b4e20eee1eaddc4d15f1a33d
Cicadaes <cicadaes@sina.com>
2018-02-09 22:21:57 -0800

    Css modules

--- templates/rollup/src/{{#name}}.vue
+++ templates/rollup/src/{{#name}}.vue
@@ -1,16 +1,23 @@
 <template>
-  <h3>{{ text }}</h3>
+  <div :class="styles.container">{{ text }}</div>
 </template>
 
 <script>
+import styles from './<#=name#>.styl'
+
 export default {
   name: '<#=name#>',
   props: {
       text: String
+  },
+  data() {
+    return {
+      styles: {
+        container: styles.container
+      }
+    }
   }
 }
 </script>
-
-<style lang="stylus" src="./<#=name#>.styl"></style>
 
 
