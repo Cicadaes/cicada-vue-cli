f27112846511002b8d86917620edacd7f736c4b0
Cicadaes <cicadaes@sina.com>
2018-02-10 08:13:48 -0800

    Add task build:style

---
+++ templates/rollup-gulp/gulp/tasks/build.style.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const gulp = require('gulp')
+const stylus = require('gulp-stylus')
+const sourcemaps = require('gulp-sourcemaps')
+
+const config = require('../config')
+
+gulp.task('build:style', function () {
+  return gulp.src(config.style.entry)
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(config.style.dest))
+})
